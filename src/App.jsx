a6973d94fa0e23188aaafcc2e210f22864d23d62
mightyee <55863723+mightyee@users.noreply.github.com>
2025-01-09 11:53:54 +0800

    dist

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,15 @@
 import './App.css';
-import Header from './components/Common/Header/Header.jsx';
-import FloatingButton from './components/FloatingButton/FloatingButton';
-import Footer from './components/Common/Footer/Footer';
-import Hero from './components/Hero/Hero';
-import SearchBar from './components/SearchBar/SearchBar';
-import Course from './components/Course/Course';
+
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from './Home';
 
 function App() {
   return (
-    <div>
-      <Header />
-      <Hero />
-      <SearchBar />
-      <Course />
-      <FloatingButton />
-      <Footer />
-    </div>
+    <Router>
+      <Routes>
+        <Route path='/' element={<Home />} />
+      </Routes>
+    </Router>
   );
 }
 
---
+++ src/Home.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import Header from './components/Common/Header/Header.jsx';
+import FloatingButton from './components/FloatingButton/FloatingButton';
+import Footer from './components/Common/Footer/Footer';
+import Hero from './components/Hero/Hero';
+import SearchBar from './components/SearchBar/SearchBar';
+import Course from './components/Course/Course';
+
+const Home = () => {
+  return (
+    <>
+      <Header />
+      <Hero />
+      <SearchBar />
+      <Course />
+      <FloatingButton />
+      <Footer />
+    </>
+  );
+};
+
+export default Home;
