70a152008635590ebed60dd060f29a0c66b26121
mightyee <55863723+mightyee@users.noreply.github.com>
2025-01-09 12:05:30 +0800

    dist

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,22 @@
 import './App.css';
+import Header from './components/Common/Header/Header.jsx';
+import FloatingButton from './components/FloatingButton/FloatingButton';
+import Footer from './components/Common/Footer/Footer';
+import Hero from './components/Hero/Hero';
+import SearchBar from './components/SearchBar/SearchBar';
+import Course from './components/Course/Course';
 
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
-import Home from './Home';
-
-const App = () => {
+function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path='/' element={<Home />} />
-      </Routes>
-    </Router>
+    <div>
+      <Header />
+      <Hero />
+      <SearchBar />
+      <Course />
+      <FloatingButton />
+      <Footer />
+    </div>
   );
-};
+}
 
 export default App;
