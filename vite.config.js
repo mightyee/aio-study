9e4f251d2583e64790ab8d8bb0e728418536f819
mightyee <55863723+mightyee@users.noreply.github.com>
2025-01-09 11:38:11 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,23 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   base: '/aio-study',
+// });
+
+
 export default defineConfig({
   plugins: [react()],
   base: '/aio-study',
+
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        404: resolve(__dirname, 'public/404.html'),
+      },
+    },
+  },
 });
